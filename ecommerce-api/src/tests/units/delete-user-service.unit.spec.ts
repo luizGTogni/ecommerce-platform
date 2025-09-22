8329606d6453fd926ae08086cf66da5fe1934f62
Luiz Togni <contato.togni@gmail.com>
2025-09-22 16:43:52 -0300

    test: testing delete user service unit

---
+++ ecommerce-api/src/tests/units/delete-user-service.unit.spec.ts
@@ -0,0 +1,43 @@
+import { ResourceNotFoundError } from "@/errors/resource-not-found.error.js";
+import { InMemoryUsersRepository } from "@/models/repositories/in-memory/users-repository.in-memory.js";
+import { IUsersRepository } from "@/models/repositories/interfaces/users-repository.interface.js";
+import { DeleteUserService } from "@/services/users/delete-user.service.js";
+import { hash } from "bcryptjs";
+import { beforeEach, describe, expect, it, MockInstance, vi } from "vitest";
+
+let usersRepository: IUsersRepository;
+let sut: DeleteUserService;
+let spyFindById: MockInstance;
+
+describe("Delete User Service (Unit)", () => {
+  beforeEach(() => {
+    usersRepository = new InMemoryUsersRepository();
+    sut = new DeleteUserService(usersRepository);
+    spyFindById = vi.spyOn(usersRepository, "findById");
+  });
+
+  it("should be able to delete a user", async () => {
+    const userCreated = await usersRepository.create({
+      name: "John Doe",
+      email: "johndoe@example.com",
+      password_hash: await hash("123456", 6),
+    });
+
+    await sut.execute({
+      userId: userCreated.id,
+    });
+
+    const user = await usersRepository.findById(userCreated.id);
+
+    expect(spyFindById).toHaveBeenCalledWith(userCreated.id);
+    expect(user).toEqual(null);
+  });
+
+  it("should not be able to delete a user if user not exists", async () => {
+    await expect(() =>
+      sut.execute({
+        userId: "user_not_exists",
+      }),
+    ).rejects.toBeInstanceOf(ResourceNotFoundError);
+  });
+});
