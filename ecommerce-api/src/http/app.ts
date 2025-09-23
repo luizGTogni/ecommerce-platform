e40cda2270474fd51ac1569fe13c8ac4eb67701c
Luiz Togni <contato.togni@gmail.com>
2025-09-23 16:44:43 -0300

    feat: add fastify server

---
+++ ecommerce-api/src/http/app.ts
@@ -0,0 +1,18 @@
+import fastify from "fastify";
+import { errorHandler } from "./middlewares/error.middleware.js";
+import { env } from "@/configs/env.js";
+import { usersRoutes } from "./routes/users.route.js";
+
+export const app = fastify();
+
+app.register(usersRoutes);
+
+app.setErrorHandler(async (error, _, reply) => {
+  if (env.NODE_ENV !== "production") {
+    console.log(error);
+  }
+
+  const response = await errorHandler(error);
+
+  return reply.status(response.statusCode).send(response.body);
+});
