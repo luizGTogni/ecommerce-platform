d8075e950548bce605f2ef0b3fd1ad5b2100aefd
Luiz Togni <contato.togni@gmail.com>
2025-09-23 16:37:38 -0300

    feat: add make authenticate service factory

--- ecommerce-api/src/services/users/factories/make-authenticate.factory.ts
+++ ecommerce-api/src/services/users/factories/make-authenticate.factory.ts
@@ -2,7 +2,7 @@ import { BcryptHasherDriver } from "@/drivers/hasher/bcrypt.hasher.js";
 import { PrismaUsersRepository } from "@/models/repositories/prisma/users-repository.prisma.js";
 import { AuthenticateService } from "../authenticate.service.js";
 
-export function makeAuthenticateFactory() {
+export function makeAuthenticateService() {
   const usersRepository = new PrismaUsersRepository();
   const hasherDriver = new BcryptHasherDriver();
   const useCase = new AuthenticateService(usersRepository, hasherDriver);
