45cc588ab6501e2d9f17efc8a017af58e54edf9f
Luiz Togni <contato.togni@gmail.com>
2025-09-24 16:24:48 -0300

    feat: add authenticate controller

--- ecommerce-api/src/http/app.ts
+++ ecommerce-api/src/http/app.ts
@@ -1,3 +1,5 @@
+import fastifyJwt from "@fastify/jwt";
+import fastifyCookie from "@fastify/cookie";
 import { env } from "@/configs/env.js";
 import fastify from "fastify";
 import { errorHandler } from "./middlewares/error.middleware.js";
@@ -5,14 +7,27 @@ import { usersRoutes } from "./routes/users.route.js";
 
 export const app = fastify();
 
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: "refreshToken",
+    signed: false,
+  },
+  sign: {
+    expiresIn: "15m",
+  },
+});
+
+app.register(fastifyCookie);
+
 app.register(usersRoutes);
 
-app.setErrorHandler(async (error, _, reply) => {
+app.setErrorHandler(async (error, request, reply) => {
   if (env.NODE_ENV !== "production") {
     console.log(error);
   }
 
-  const response = await errorHandler(error);
+  const response = await errorHandler(error, request, reply);
 
   return reply.status(response.statusCode).send(response.body);
 });
