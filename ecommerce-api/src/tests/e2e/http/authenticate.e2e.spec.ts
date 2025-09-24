9abe71aa0975d5e9b6646430af251c8b1bb47ea9
Luiz Togni <contato.togni@gmail.com>
2025-09-24 16:32:47 -0300

    test: testing authenticate end to end

---
+++ ecommerce-api/src/tests/e2e/http/authenticate.e2e.spec.ts
@@ -0,0 +1,31 @@
+import { app } from "@/http/app.js";
+import request from "supertest";
+import { describe, afterAll, beforeAll, it, expect } from "vitest";
+
+describe("Authenticate (e2e)", () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it("should be able to authenticate", async () => {
+    await request(app.server).post("/users").send({
+      name: "John Doe",
+      email: "johndoe@example.com",
+      password: "123456",
+    });
+
+    const response = await request(app.server).post("/sessions").send({
+      email: "johndoe@example.com",
+      password: "123456",
+    });
+
+    expect(response.statusCode).toEqual(200);
+    expect(response.body).toEqual({
+      token: expect.any(String),
+    });
+  });
+});
