1b7883078530c3236f308546b6da0dc407339d70
Luiz Togni <contato.togni@gmail.com>
2025-09-24 22:36:15 -0300

    refactor: change seva refresh token service [unfinished]

--- ecommerce-api/src/services/users/save-refresh-token.service.ts
+++ ecommerce-api/src/services/users/save-refresh-token.service.ts
@@ -1,22 +1,30 @@
+import type { IHasher } from "@/drivers/interfaces/hasher.interface.js";
 import { ResourceNotFoundError } from "@/errors/resource-not-found.error.js";
-import { User } from "@/models/entities/user.entity.js";
-import { IUsersRepository } from "@/models/repositories/interfaces/users-repository.interface.js";
+import type { Session } from "@/models/entities/session.entity.js";
+import type { ISessionsRepository } from "@/models/repositories/interfaces/sessions-repository.interface.js";
+import type { IUsersRepository } from "@/models/repositories/interfaces/users-repository.interface.js";
 
 type SaveRefreshTokenRequest = {
   userId: string;
   refreshToken: string;
+  expiresAt: Date;
 };
 
 type SaveRefreshTokenResponse = {
-  user: User;
+  session: Session;
 };
 
 export class SaveRefreshTokenService {
-  constructor(private readonly usersRepository: IUsersRepository) {}
+  constructor(
+    private readonly usersRepository: IUsersRepository,
+    private readonly sessionsRepository: ISessionsRepository,
+    private readonly hasherDriver: IHasher,
+  ) {}
 
   async execute({
     userId,
     refreshToken,
+    expiresAt,
   }: SaveRefreshTokenRequest): Promise<SaveRefreshTokenResponse> {
     const user = await this.usersRepository.findById(userId);
 
@@ -24,10 +32,26 @@ export class SaveRefreshTokenService {
       throw new ResourceNotFoundError();
     }
 
-    user.session_hash = refreshToken;
+    const session_current =
+      await this.sessionsRepository.findByUserIdAndRevoked({
+        userId,
+        isRevoked: false,
+      });
 
-    await this.usersRepository.save(user);
+    if (session_current) {
+      session_current.revoked = true;
+      await this.sessionsRepository.save(session_current);
+    }
 
-    return { user };
+    const session = await this.sessionsRepository.create({
+      user_id: userId,
+      token_hash: await this.hasherDriver.hash({
+        plain: refreshToken,
+        salt: 8,
+      }),
+      expires_at: expiresAt,
+    });
+
+    return { session };
   }
 }
