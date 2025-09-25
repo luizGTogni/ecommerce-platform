1853e1e996730bf313a44313f83b83b63822124e
Luiz Togni <contato.togni@gmail.com>
2025-09-25 14:29:17 -0300

    feat: add prisma session repository

---
+++ ecommerce-api/src/models/repositories/prisma/sessions-repository.prisma.ts
@@ -0,0 +1,40 @@
+import type { SessionCreate } from "@/models/entities/dto/session-create.dto.js";
+import type { Session } from "@/models/entities/session.entity.js";
+import type {
+  IFindByUserIdAndRevokedProps,
+  ISessionsRepository,
+} from "../interfaces/sessions-repository.interface.js";
+import { prisma } from "@/configs/prisma.js";
+
+export class PrismaSessionsRepository implements ISessionsRepository {
+  async create(data: SessionCreate) {
+    const session = await prisma.session.create({
+      data,
+    });
+
+    return session;
+  }
+
+  async save(sessionData: Session) {
+    await prisma.session.update({
+      where: {
+        id: sessionData.id,
+      },
+      data: {
+        revoked: sessionData.revoked,
+      },
+    });
+  }
+
+  async findByUserIdAndRevoked({
+    userId,
+    isRevoked,
+  }: IFindByUserIdAndRevokedProps) {
+    return await prisma.session.findFirst({
+      where: {
+        user_id: userId,
+        revoked: isRevoked,
+      },
+    });
+  }
+}
