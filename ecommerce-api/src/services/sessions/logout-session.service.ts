e07d9cdefd00684dded18e9ee4856540c87a75bc
Luiz Togni <contato.togni@gmail.com>
2025-09-26 16:32:45 -0300

    feat: add logout session service

---
+++ ecommerce-api/src/services/sessions/logout-session.service.ts
@@ -0,0 +1,45 @@
+import { SessionInvalidError } from "@/errors/session-invalid.error.js";
+import { Session } from "@/models/entities/session.entity.js";
+import { IMemcacheRepository } from "@/models/repositories/interfaces/memcache-repository.interface.js";
+import { ISessionsRepository } from "@/models/repositories/interfaces/sessions-repository.interface.js";
+
+type LogoutSessionRequest = {
+  userId: string;
+  token: string;
+};
+
+type LogoutSessionResponse = {
+  session: Session;
+};
+
+export class LogoutSessionService {
+  constructor(
+    private readonly sessionsRepository: ISessionsRepository,
+    private readonly memcacheRepository: IMemcacheRepository,
+  ) {}
+
+  async execute({
+    userId,
+    token,
+  }: LogoutSessionRequest): Promise<LogoutSessionResponse> {
+    const session = await this.sessionsRepository.findByUserIdAndRevoked({
+      userId,
+      isRevoked: false,
+    });
+
+    if (!session) {
+      throw new SessionInvalidError();
+    }
+
+    session.revoked = true;
+    await this.sessionsRepository.save(session);
+
+    await this.memcacheRepository.set({
+      key: `revoked_list_${session.id}`,
+      value: token,
+      configs: { EX: 900 },
+    });
+
+    return { session };
+  }
+}
