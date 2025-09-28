fc884576245fa7907efa9e35bc7fca71bc2e9660
Luiz Togni <contato.togni@gmail.com>
2025-09-28 11:39:42 -0300

    feat: add verify if revoked token in verify jwt

--- ecommerce-api/src/http/middlewares/verify-jwt.middleware.ts
+++ ecommerce-api/src/http/middlewares/verify-jwt.middleware.ts
@@ -1,8 +1,25 @@
+import { makeVerifyIfRevokedTokenService } from "@/services/sessions/factories/make-verify-if-revoked-token.factory.js";
 import { FastifyReply, FastifyRequest } from "fastify";
 
 export async function verifyJWT(request: FastifyRequest, reply: FastifyReply) {
   try {
     await request.jwtVerify();
+
+    const tokenRequest = request.headers.authorization;
+
+    if (!tokenRequest) {
+      return reply.status(401).send({ message: "Unauthorized." });
+    }
+
+    const verifyIfRevokedList = makeVerifyIfRevokedTokenService();
+    const { isRevoked } = await verifyIfRevokedList.execute({
+      userId: request.user.sub,
+      token: tokenRequest.replace("Bearer ", ""),
+    });
+
+    if (isRevoked) {
+      return reply.status(401).send({ message: "Unauthorized." });
+    }
   } catch {
     return reply.status(401).send({ message: "Unauthorized." });
   }
