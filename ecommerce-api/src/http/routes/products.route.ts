cda21f56886422f6985409dbcb87532ffff21e08
Luiz Togni <contato.togni@gmail.com>
2025-09-29 16:20:44 -0300

    feat: add edit product route

--- ecommerce-api/src/http/routes/products.route.ts
+++ ecommerce-api/src/http/routes/products.route.ts
@@ -1,11 +1,18 @@
 import { createProduct } from "../controllers/products/create.controller.js";
+import { editProduct } from "../controllers/products/edit.controller.js";
 
 import type { FastifyTypeInstance } from "../interfaces/fastify-types.js";
 import { verifyJWT } from "../middlewares/verify-jwt.middleware.js";
 import { createProductSchema } from "../schemas/docs/products/docs-create.schema.js";
+import { editProductBodySchema } from "../schemas/http/products/http-edit.schema.js";
 
 export async function productsRoutes(app: FastifyTypeInstance) {
   app.addHook("onRequest", verifyJWT);
 
   app.post("/products", { schema: createProductSchema }, createProduct);
+  app.put(
+    "/products/:productId",
+    { schema: editProductBodySchema },
+    editProduct,
+  );
 }
