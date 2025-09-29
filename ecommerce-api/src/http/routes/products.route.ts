5b93d951d97ff182a904307471b7075ba4fc7c16
Luiz Togni <contato.togni@gmail.com>
2025-09-29 16:45:52 -0300

    feat: add search products route

--- ecommerce-api/src/http/routes/products.route.ts
+++ ecommerce-api/src/http/routes/products.route.ts
@@ -1,15 +1,18 @@
 import { createProduct } from "../controllers/products/create.controller.js";
 import { editProduct } from "../controllers/products/edit.controller.js";
+import { searchProducts } from "../controllers/products/search-products.controller.js";
 
 import type { FastifyTypeInstance } from "../interfaces/fastify-types.js";
 import { verifyJWT } from "../middlewares/verify-jwt.middleware.js";
 import { createProductSchema } from "../schemas/docs/products/docs-create.schema.js";
+import { searchProductsSchema } from "../schemas/docs/products/docs-search-products.schema.js";
 import { editProductBodySchema } from "../schemas/http/products/http-edit.schema.js";
 
 export async function productsRoutes(app: FastifyTypeInstance) {
   app.addHook("onRequest", verifyJWT);
 
   app.post("/products", { schema: createProductSchema }, createProduct);
+  app.get("/products", { schema: searchProductsSchema }, searchProducts);
   app.put(
     "/products/:productId",
     { schema: editProductBodySchema },
