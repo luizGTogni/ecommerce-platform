eaf82a590dd0bb49ab1b9a93794ab04d801e4a8c
Luiz Togni <contato.togni@gmail.com>
2025-09-29 21:26:14 -0300

    feat: add get product controller

---
+++ ecommerce-api/src/http/controllers/products/get-product.controller.ts
@@ -0,0 +1,20 @@
+import { getProductParamsSchema } from "@/http/schemas/http/products/http-get-product.schema.js";
+import { makeGetProductService } from "@/services/products/factories/make-get-product-service.factory.js";
+import type { FastifyReply, FastifyRequest } from "fastify";
+
+export async function getProduct(request: FastifyRequest, reply: FastifyReply) {
+  const { productId } = getProductParamsSchema.parse(request.params);
+
+  try {
+    const service = makeGetProductService();
+
+    const { product } = await service.execute({
+      productId,
+      hasViewerPermission: false,
+    });
+
+    return reply.status(200).send({ product });
+  } catch (err) {
+    throw err;
+  }
+}
