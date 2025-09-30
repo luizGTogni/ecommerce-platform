b4962618982e83d3ff58ecdbed71d438ed47aace
Luiz Togni <contato.togni@gmail.com>
2025-09-30 16:32:35 -0300

    test: testing create cart service unit

---
+++ ecommerce-api/src/tests/units/create-cart-service.unit.spec.ts
@@ -0,0 +1,69 @@
+import { ResourceAlreadyExistsError } from "@/errors/resource-already-exists.error.js";
+import { ResourceNotFoundError } from "@/errors/resource-not-found.error.js";
+import { InMemoryCartsRepository } from "@/models/repositories/in-memory/carts-repository.in-memory.js";
+import { InMemoryUsersRepository } from "@/models/repositories/in-memory/users-repository.in-memory.js";
+import { ICartsRepository } from "@/models/repositories/interfaces/carts-repository.interface.js";
+import { IUsersRepository } from "@/models/repositories/interfaces/users-repository.interface.js";
+import { CreateCartService } from "@/services/carts/create-cart.service.js";
+import { hash } from "bcryptjs";
+import { beforeEach, describe, expect, it } from "vitest";
+
+let cartsRepository: ICartsRepository;
+let usersRepository: IUsersRepository;
+let sut: CreateCartService;
+
+describe("Create Cart Service (Unit)", () => {
+  beforeEach(() => {
+    cartsRepository = new InMemoryCartsRepository();
+    usersRepository = new InMemoryUsersRepository();
+    sut = new CreateCartService(cartsRepository, usersRepository);
+  });
+
+  it("should be able to create a cart", async () => {
+    const user = await usersRepository.create({
+      name: "John Doe",
+      email: "johndoe@example.com",
+      password_hash: await hash("123456", 6),
+    });
+
+    const response = await sut.execute({
+      userId: user.id,
+    });
+
+    expect(response.cart).toEqual(
+      expect.objectContaining({
+        id: expect.any(String),
+        user_id: user.id,
+        status: "OPEN",
+        finished_at: null,
+        created_at: expect.any(Date),
+      }),
+    );
+  });
+
+  it("should not be able to create a cart if user not exists", async () => {
+    await expect(() =>
+      sut.execute({
+        userId: "user-not-exists",
+      }),
+    ).rejects.toBeInstanceOf(ResourceNotFoundError);
+  });
+
+  it("should not be able to create a cart if cart already exists", async () => {
+    const user = await usersRepository.create({
+      name: "John Doe",
+      email: "johndoe@example.com",
+      password_hash: await hash("123456", 6),
+    });
+
+    await sut.execute({
+      userId: user.id,
+    });
+
+    await expect(() =>
+      sut.execute({
+        userId: user.id,
+      }),
+    ).rejects.toBeInstanceOf(ResourceAlreadyExistsError);
+  });
+});
