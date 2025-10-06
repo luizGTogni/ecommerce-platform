51b2d1790b53fdd9b8e243d9e90c5077f4da5233
Luiz Togni <contato.togni@gmail.com>
2025-10-06 13:38:12 -0300

    refactor: add api version stripe

--- ecommerce-api/src/configs/stripe.ts
+++ ecommerce-api/src/configs/stripe.ts
@@ -2,5 +2,5 @@ import Stripe from "stripe";
 import { env } from "./env.js";
 
 export const stripe = new Stripe(env.STRIPE_SECRET_KEY as string, {
-  apiVersion: "",
+  apiVersion: "2025-09-30.clover",
 });
